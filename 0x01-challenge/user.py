c09962a1d72ae768a711f9356d1beffd0b9d0c60
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-18 21:05:13 +1000

    user fixed

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
+
 
 class User():
     """ Documentation """
@@ -10,19 +11,19 @@ class User():
         """ Documentation """
         self.__email = None
 
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
-
     @property
     def email(self):
         """ Documentation """
         return self.__email
-   
-    
+
+    @email.setter
+    def email(self, value):
+        """ Documentation """
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
 if __name__ == "__main__":
 
     u = User()
