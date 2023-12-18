e7c327f6a9adad6dd4a1bceafdb684a579c7b4a0
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-18 21:06:24 +1000

    square fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
